24e442ea207429da2ac0e056ea3589a735d5ee97
Itamar Yarden <itamary9@gmail.com>
2020-07-20 13:36:43 +0300

    ProtoType

--- smart_fit-proj/src/static/SmartFit/js/main.js
+++ smart_fit-proj/src/static/SmartFit/js/main.js
@@ -11,6 +11,20 @@ $('#showmore').click(function () {
 
 //---------------------------Workout form Trainer---------------//
 
+if( document.getElementById("duplicator3") != null)
+    var updateClicks = 3;
+
+if( document.getElementById("duplicator2") != null)
+      var updateClicks = 2;
+else
+    var updateClicks = 1;
+
+
+var numOfClicks = 1;
+var original = document.getElementById("duplicator1");
+if (original != null)
+  var clones = [original];
+
 var numOfClicks = 1;
 var original = document.getElementById("duplicator1");
 if (original != null)
@@ -18,7 +32,7 @@ if (original != null)
 
 $(".addNewEx").click(function () {
   if (numOfClicks <= 2) {
-    
+
     var clone = original.cloneNode(true);
     clone.childNodes[9].childNodes[1].value = "";
     clone.childNodes[3].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_name";
@@ -34,21 +48,23 @@ $(".addNewEx").click(function () {
 })
 
 
+
 $(".createmore").click(function () {
-  if( document.getElementById("duplicator2") != null)
-      numOfClicks++;
-  if (numOfClicks <= 2) {
+
+
+  if (updateClicks <= 2) {
     var clone = original.cloneNode(true);
     clone.childNodes[9].childNodes[1].value = "";
-    clone.childNodes[3].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_name";
-    clone.childNodes[5].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_sets";
-    clone.childNodes[7].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_reps";
-    clone.childNodes[9].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_desc";
-    clone.id = "duplicator" + ((numOfClicks)+1);
-    var txt = '<h6>Ex' + (numOfClicks +1 ) +  '</h6>';
+    clone.childNodes[3].childNodes[1].name = "ex_" + (updateClicks + 1) + "_name";
+    clone.childNodes[5].childNodes[1].name = "ex_" + (updateClicks + 1) + "_sets";
+    clone.childNodes[7].childNodes[1].name = "ex_" + (updateClicks + 1) + "_reps";
+    clone.childNodes[9].childNodes[1].name = "ex_" + (updateClicks + 1) + "_desc";
+    clone.id = "duplicator" + ((updateClicks)+1);
+    var txt = '<h6>Ex' + (updateClicks +1 ) +  '</h6>';
     clone.childNodes[1].innerHTML = txt;
     clones.push(clone);
     original.parentNode.appendChild(clone);
+    updateClicks += 1;
   }
 })
 
@@ -66,5 +82,4 @@ if ($exList.find('option').length == 1) {
 }
 
 
-//--------------------------------Delete warning-------------------//
 
